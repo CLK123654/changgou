540a1a329f12171d29963cc4bf1a8f654264a35d
huanghb <1263712139@qq.com>
2020-07-11 13:16:36 +0800

    Initial commit

---
+++ changgou-common/src/main/java/com/hae/entity/Message.java
@@ -0,0 +1,75 @@
+package com.hae.entity;
+
+import com.alibaba.fastjson.annotation.JSONField;
+
+import java.io.Serializable;
+
+/*****
+ * @author huanghb
+ * @Description: entity:MQ消息封装
+ ****/
+public class Message implements Serializable{
+
+    /**
+     * 执行的操作  1：增加，2：修改,3：删除
+     */
+    private int code;
+
+
+    private Object content;
+
+
+    @JSONField(serialize = false)
+    private String routekey;
+
+
+    @JSONField(serialize = false)
+    private String exechange;
+
+    public Message() {
+    }
+
+    public Message(int code, Object content) {
+        this.code = code;
+        this.content = content;
+    }
+
+    public Message(int code, Object content, String routekey, String exechange) {
+        this.code = code;
+        this.content = content;
+        this.routekey = routekey;
+        this.exechange = exechange;
+    }
+
+    public String getRoutekey() {
+        return routekey;
+    }
+
+    public void setRoutekey(String routekey) {
+        this.routekey = routekey;
+    }
+
+    public String getExechange() {
+        return exechange;
+    }
+
+    public void setExechange(String exechange) {
+        this.exechange = exechange;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Object getContent() {
+        return content;
+    }
+
+    public void setContent(Object content) {
+        this.content = content;
+    }
+}
---
+++ changgou-eureka/src/main/java/com/hae/changgou/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.hae.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author huanghb
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaApplication.class, args);
+  }
+}
